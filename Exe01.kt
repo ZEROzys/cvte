d520f8286da50f5874a9c08903a6cec23284ebb6
ZEROzys <1538841089@qq.com>
2019-07-26 09:41:28 +0800

    增加注释，降低时间复杂度

--- Exe01.kt
+++ Exe01.kt
@@ -1,21 +1,50 @@
-fun findKthLarge(arr: IntArray, k: Int): Int {
-    // k次冒泡
-    var i = 0
-    while (i < k) {
-        for (index in arr.indices-arr.lastIndex) {
-            if (arr[index] > arr[index+1]) {
-                var temp = arr[index]
-                arr[index] = arr[index+1]
-                arr[index+1] = temp
-            }
+﻿//fun findKthLarge(arr: IntArray, k: Int): Int {
+//    // k次冒泡
+//    var i = 0
+//    while (i < k) {
+//        for (index in arr.indices-arr.lastIndex) {
+//            if (arr[index] > arr[index+1]) {
+//                var temp = arr[index]
+//                arr[index] = arr[index+1]
+//                arr[index+1] = temp
+//            }
+//        }
+//        i ++
+//    }
+//    return arr[arr.size-k]
+//}
+
+//  快排划分
+fun partition(arr: IntArray, left: Int, right: Int): Int {
+    val base = arr[left]
+    var l = left
+    var r = right
+    while (l < r) {
+        while (l < r && arr[r] >= base) r --;
+        if (arr[r] < base) {
+            arr[l] = arr[r]
+            l++
         }
-        i ++
+        while (l < r && arr[l] <= base) l ++;
+        if (arr[l] > base) {
+            arr[r] = arr[l]
+            r --
+        }
     }
-    return arr[arr.size-k]
+    arr[l] = base
+    return l
+}
+
+fun findKthLargeWithQS(arr: IntArray, left: Int, right: Int, k: Int): Int {
+    var index = partition(arr, left, right)
+    if (index == arr.size-k) return arr[index]
+    else if (index < arr.size-k) return findKthLargeWithQS(arr, index+1, right, k)
+    else return findKthLargeWithQS(arr, left, index-1, k)
 }
 
 fun main() {
-    val arr = intArrayOf(3, 2, 3, 1, 2, 4, 5, 5, 6)
-    val k = 4
-    println(findKthLarge(arr, k))
+    val arr = intArrayOf(3, 2, 3, 1, 2, 4, 3, 5, 6)
+    val k = 9
+    println(findKthLargeWithQS(arr, 0, arr.size-1, k))
+//    println(findKthLarge(arr, k))
 }
--- Exe02.kt
+++ Exe02.kt
@@ -4,12 +4,19 @@ class StockSpanner (val arr: IntArray) {
     fun getSpannerArray(): IntArray {
         var spannerArray = IntArray(arr.size)
         spannerArray[0] = 1
+
+        //  维护一个单调栈
         val stack = LinkedList<Pair<Int, Int>>()
         stack.add(0 to arr[0])
         for (i in arr.indices-0) {
+            //  当前元素比栈顶元素大，栈顶元素出栈
             while (arr[i] > stack.peekLast().second) {
                 stack.pollLast()
             }
+            /*
+             * 两种情况，当前元素与栈顶元素相等，则当前元素的股票跨度为栈顶元素的跨度+1
+             * 当前元素比栈顶元素小，跨度为当前元素与栈顶元素的索引之差
+             */
             if (arr[i] == stack.peekLast().second)
                 spannerArray[i] = spannerArray[i-1] + 1
             else
@@ -23,7 +30,7 @@ class StockSpanner (val arr: IntArray) {
 
 fun main() {
 //    val arr = intArrayOf(100, 80, 60, 70, 60, 75, 85)
-    val arr = intArrayOf(100, 100, 100, 100, 100, 75, 85)
+    val arr = intArrayOf(100, 88, 100, 100, 100, 75, 85)
     val stockSpanner = StockSpanner(arr)
     val res = stockSpanner.getSpannerArray()
     for (i in res.indices) {
